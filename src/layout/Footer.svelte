36999740e431bb76e1b1b8ca3ead33ebee6ab299
mhogeveen <maarten@maartens-air.lan>
2021-07-10 13:50:26 +0200

    Add footer component

---
+++ src/layout/Footer.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+
+</script>
+
+<footer class='footer container'>
+  <p class='footer__content'>{`MDocs ${new Date().getFullYear()}`}</p>
+</footer>
+
+<style lang='scss'>
+  .footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+
+    &__content {
+      margin-bottom: 0;
+    }
+  }
+</style>
