c5924ec8ac3f478d10c14a1ddb101f9669f9c2c3
mhogeveen <maarten@maartens-air.lan>
2021-07-10 13:50:46 +0200

    Strip out unnecessary content of index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,22 +1,11 @@
 <script lang="ts">
-  let count: number = 0;
-
-  const handleClick = () => {
-    count++;
-  }
 
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button on:click={handleClick}>{count}</button>
+<main>
+  <h1>MDocs</h1>
+</main>
 
 <style lang="scss">
-  p {
-    font-weight: bold;
-
-    a {
-      color: red;
-    }
-  }
+  
 </style>
