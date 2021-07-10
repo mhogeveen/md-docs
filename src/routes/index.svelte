396dfab7f6667067b146c86304d09672fb148426
mhogeveen <maarten@maartens-air.lan>
2021-07-10 11:14:53 +0200

    Try if sass works

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,22 @@
+<script lang="ts">
+  let count: number = 0;
+
+  const handleClick = () => {
+    count++;
+  }
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={handleClick}>{count}</button>
+
+<style lang="scss">
+  p {
+    font-weight: bold;
+
+    a {
+      color: red;
+    }
+  }
+</style>
